f4088f1cce11a2c6b9aa73686aabbb7e31c2fa24
runonwalls <hmorgan471@gmail.com>
2015-03-05 06:15:45 -0800

    finished the lab

--- lab2.js
+++ lab2.js
@@ -48,26 +48,65 @@ function assert(expression, failureMessage) {
  with Dowington.
 */
 
-var hoursSpentInDowington; // TODO: assign me the value of the
-                           // above calculation
+//Keeping individual segments outside of the global space
+(function() {
 
+function Blob() {
+  this.peopleConsumed = 0;
+  this.hoursAlive = 0;
+}
+
+var blob = new Blob();
+
+//While there are still people in the town
+while (blob.peopleConsumed < 1000)
+{
+  //Eat at our rate of consumption
+  blob.peopleConsumed += (blob.peopleConsumed + 1);
+  blob.hoursAlive++;
+}
+
+var hoursSpentInDowington = blob.hoursAlive;
+                           // TODO: assign me the value of the
+                           // above calculation
 // Now, write a method that takes a population for an arbitrary
 // town, and the starting consumption rate, and returns the number
 // of hours the blob needs to ooze its way through that town.
 
 function hoursToOoze(population, peoplePerHour) {
   // TODO: implement me based on the instructions above. Be sure to then assign me to the Blob's prototype.
+  if (peoplePerHour <= 0)
+  {
+    return Infinity;
+  }
+
+  var peopleConsumed = 0, hours = 0;
+  while (peopleConsumed < population)
+  {
+    peopleConsumed += peoplePerHour;
+    peoplePerHour += peoplePerHour;
+    hours++;
+  }
+  return hours;
 }
+Blob.prototype.hoursToOoze = hoursToOoze;
 
 assert(blob.hoursToOoze(0, 1) === 0, "no people means no time needed.");
 assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
   "hoursSpentInDowington should match hoursToOoze\"s result for 1000");
 // TODO: write three more assertions like the two above, testing out
 // the hoursToOoze method.
+assert(blob.hoursToOoze(1000, 1000) === 1, "should take one hour when population equals people per hour");
+assert(blob.hoursToOoze(1, 0) === Infinity, "the blob will be unable to finish if it cannot consume people");
+assert(blob.hoursToOoze(1, -1) === Infinity, "the blob will be unable to finish if it actually CREATES people");
+
+})(); //End of this section!
 
 //*********************************************************
 // PROBLEM 2: Universal Translator. 20 points
 //*********************************************************
+
+(function() {
 
 var hello = {
   klingon: "nuqneH",  // home planet is Qo"noS
@@ -79,9 +118,11 @@ var hello = {
 // sentient beings. They have a home planet, a language that they
 // speak, and method called sayHello.
 
-function SentientBeing () {
+function SentientBeing (home, lang) {
   // TODO: specify a home planet and a language
   // you'll need to add parameters to this constructor
+  this.homePlanet = home;
+  this.language = lang;
 }
 
 // sb is a SentientBeing object
@@ -91,17 +132,41 @@ function sayHello (sb) {
     // of the listener (the sb parameter above).
     // use the 'hello' object at the beginning of this exercise
     // to do the translating
-
+    console.log(hello[this.language]);
+    return hello[sb.language];
     //TODO: put this on the SentientBeing prototype
   }
+SentientBeing.prototype.sayHello = sayHello;
 
 // TODO: create three subclasses of SentientBeing, one for each
 // species above (Klingon, Human, Romulan).
+function Human() {}
+Human.prototype = new SentientBeing("Earth", "federation standard");
+
+function Klingon() {}
+Klingon.prototype = new SentientBeing("Qo\"noS", "klingon");
+
+function Romulan() {}
+Romulan.prototype = new SentientBeing("Romulus", "romulan");
 
 assert((new Human()).sayHello(new Klingon()) === "nuqneH",
   "the klingon should hear nuqneH");
 // TODO: write five more assertions, to complete all the possible
 // greetings between the three types of sentient beings you created above.
+assert((new Human()).sayHello(new Romulan()) === "Jolan\"tru",
+  "the romulan should hear Jolan\"tru");
+
+assert((new Klingon()).sayHello(new Romulan()) === "Jolan\"tru",
+  "the romulan should hear Jolan\"tru");
+assert((new Klingon()).sayHello(new Human()) === "hello",
+  "the human should hear hello");
+
+assert((new Romulan()).sayHello(new Klingon()) === "nuqneH",
+  "the klingon should hear nuqneH");
+assert((new Romulan()).sayHello(new Human()) === "hello",
+  "the human should hear hello");
+
+})();//End of this section!
 
 //*********************************************************
 // PROBLEM 3: Sorting. 20 points.
@@ -110,28 +175,117 @@ assert((new Human()).sayHello(new Klingon()) === "nuqneH",
 // assertions for each one
 //*********************************************************
 function lastLetterSort(stringArray) {
-  function byLastLetter(array) {
+  function byLastLetter(a, b) {
     //TODO: implement me. sort the strings in alphabetical
     // order using their last letter
     // read this: http://www.w3schools.com/jsref/jsref_sort.asp
+    return a.slice(-1).toLowerCase().localeCompare(b.slice(-1).toLowerCase());
+    //localeCompare returns -1 if a should go first, 0 if they're the same, and 1 if b should go first.
   }
   stringArray.sort(byLastLetter);
 }
 
+//Testing!
+(function() {
+var array1 = [ "apple", "banana", "pear", "mountain", "john", "paul", "george", "ringo" ];
+lastLetterSort(array1);
+var array2 = [ "a", "a", "z", "z", "az", "za", "ZA", "AZ", "ZZZ", "AAA", "b", "a", "q",
+              "SFLKS", "jgfkjewoi", "for", "the", "love", "of", "god", "montressor" ];
+lastLetterSort(array2);
+
+function isArrayLastLetterSorted(stringArray) {
+  if (stringArray.length <= 1)
+  {
+    return true;
+  }
+
+  for (var i = 1; i < stringArray.length; ++i)
+  {
+    //If any element's last character is less than the last character of the element preceding it
+    if (stringArray[i].slice(-1).toLowerCase() < stringArray[i - 1].slice(-1).toLowerCase())
+    {
+      return false;
+    }
+  }
+  return true;
+}
+assert(isArrayLastLetterSorted(array1), "the first test array was not properly sorted.");
+assert(isArrayLastLetterSorted(array2), "the second test array was not properly sorted.");
+})();
+
+//*********************************************************
+
 function sumArray(numberArray) {
   var sum = 0;
   // TODO: implement me using forEach
+  numberArray.forEach(function(currVal) {sum += currVal;});
   return sum;
 }
 
+//Testing!
+(function() {
+var array1 = [ 1, 2, 3, -1 ];
+var mySum1 = sumArray(array1);
+
+var array2 = [ 1, -1, 400, -399, -1 ];
+var mySum2 = sumArray(array2);
+
+var array3 = [ 0 ];
+var mySum3 = sumArray(array3);
+
+assert(mySum1 === 5,  "sumArray didn't correctly sum the first test array.");
+assert(mySum2 === 0,  "sumArray didn't correctly sum the second test array.");
+assert(mySum3 === 0,  "sumArray didn't correctly sum the third test array.");
+})();
+
+//*********************************************************
+
 function sumSort(arrayOfArrays) {
-  arrayOfArrays.sort(function(item) {
+  arrayOfArrays.sort(function(item1, item2) {
     // TODO: implement me using sumArray
     //  order the arrays based on the sum of the numbers
     //  inside each array
+    var sum1 = sumArray(item1);
+    var sum2 = sumArray(item2);
+    //Sort from lowest to highest.
+    if (sum1 < sum2)
+    {
+      return -1;
+    }
+    if (sum1 > sum2)
+    {
+      return 1;
+    }
+    return 0;
   });
 }
 
+//Testing!
+(function() {
+var arrayArray1 = [ [ 1, 2, 3 ], [ -20, -1, -4 ], [ 500, 1, 2 ] ];
+sumSort(arrayArray1);
+var arrayArray2 = [ [ 0, 0, 0 ], [ -10000, -20, 5000 ], [ 90, -240, 1 ] ];
+sumSort(arrayArray2);
+
+function isArrayArraySumSorted(arrayOfArrays) {
+  if (arrayOfArrays.length <= 1)
+  {
+    return true;
+  }
+
+  for (var i = 1; i < arrayOfArrays.length; ++i)
+  {
+    if (sumArray(arrayOfArrays[i]) < sumArray(arrayOfArrays[i - 1]))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+assert(isArrayArraySumSorted(arrayArray1), "the first array of arrays was not properly sum sorted");
+assert(isArrayArraySumSorted(arrayArray2), "the second array of arrays was not properly sum sorted");
+})();
 //*********************************************************
 // PROBLEM 4: Cleanup: 10 points
 // Makes sure this file passes jshint and jscs
@@ -139,3 +293,4 @@ function sumSort(arrayOfArrays) {
 // ./node_modules/.bin/grunt jshint
 // ./node_modules/.bin/grunt jscs
 //*********************************************************
+//I did those things! Is it okay if we customize jscs? (I'll probably just ask you this)
